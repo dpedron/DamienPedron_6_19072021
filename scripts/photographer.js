00e489f450102a971c03cd89e184dcc2578085b6
Damien PÉDRON <pedron.damien@outlook.fr>
2021-08-11 10:07:39 +0200

    pictures section modifications

--- scripts/photographer.js
+++ scripts/photographer.js
@@ -1,3 +1,5 @@
+let photographers = [];
+
 window.onload = function() {
 
     const photographerCard = document.createElement("section");
@@ -43,91 +45,101 @@ window.onload = function() {
     .then(response => response.json())    
     .then(function(data){
 
-        /* Photographer card creation */        
-            photographerName.innerText = data.photographers[i].name;
-            photographerLocation.innerText = data.photographers[i].city + ", " + data.photographers[i].country;
-            photographerTagline.innerText = data.photographers[i].tagline;
-            photographerTags.innerHTML = "";
-            for(j=0;j<data.photographers[i].tags.length;j++){
-                photographerTagLink.innerText = data.photographers[i].tags[j];
-                hashtag.innerText = "#";
-                photographerTagLink.prepend(hashtag);
-                photographerTag.appendChild(photographerTagLink);
-                photographerTags.appendChild(photographerTag.cloneNode(true));
-            }
-            photographerButton.innerText = "Contactez-moi";
-            photographerPortrait.src = "../images/pictures/portraits/" + data.photographers[i].portrait;
-            document.querySelector('main').appendChild(photographerCard.cloneNode(true));
-
-            /* Form modal creation */
-            const form = document.createElement("form");
-            form.id = "photographer-form";
-            const formHeading = document.createElement("h1");
-            const formFirstLabel = document.createElement("label");
-            formFirstLabel.setAttribute('for', 'first');
-            const formFirstInput = document.createElement("input");
-            formFirstInput.setAttribute('type', 'text');
-            formFirstInput.id = "first";
-            formFirstInput.setAttribute('name', 'first');
-            const formLastLabel = document.createElement("label")
-            formLastLabel.setAttribute('for', 'last');
-            const formLastInput = document.createElement("input");
-            formLastInput.setAttribute('type', 'text');
-            formLastInput.id = "last";
-            formLastInput.setAttribute('name', 'last');
-            const formEmailLabel = document.createElement("label")
-            formEmailLabel.setAttribute('for', 'email');
-            const formEmailInput = document.createElement("input");
-            formEmailInput.setAttribute('type', 'text');
-            formEmailInput.id = "email";
-            formEmailInput.setAttribute('name', 'email');
-            const formMessageLabel = document.createElement("label");
-            formMessageLabel.setAttribute('for', 'message');
-            const formMessageInput = document.createElement("textarea");
-            formMessageInput.setAttribute('type', 'text');
-            formMessageInput.id = "message";
-            formMessageInput.setAttribute('name', 'message');
-            const formSubmit = document.createElement("button");
-            formSubmit.id = "submit"
-            const formClose = document.createElement("button");
-            formClose.id = "close"
-
-            form.appendChild(formHeading);
-            form.appendChild(formFirstLabel);
-            form.appendChild(formFirstInput);    
-            form.appendChild(formLastLabel);    
-            form.appendChild(formLastInput);
-            form.appendChild(formEmailLabel);    
-            form.appendChild(formEmailInput);
-            form.appendChild(formMessageLabel);    
-            form.appendChild(formMessageInput);
-            form.appendChild(formSubmit);
-            form.appendChild(formClose);                 
-            document.querySelector('body').appendChild(form);
-            
-            formHeading.innerHTML = "Contactez-moi<br>\ " + data.photographers[i].name;
-            formFirstLabel.innerText = "Prénom";
-            formLastLabel.innerText = "Nom";
-            formEmailLabel.innerText = "Email";
-            formMessageLabel.innerText = "Votre message";
-            formSubmit.innerText = "Envoyer";
-            formClose.innerText = "X";
-
-            function openModal(){
-                form.style.display = "flex";
-                document.querySelector('main').style.display = "none";
-                document.querySelector('header').style.display = "none";
-            }
-
-            function closeModal(){
-                form.style.display = "none";
-                document.querySelector('main').style.display = "flex";
-                document.querySelector('header').style.display = "block";
-            }
         
-            document.getElementById('photographer-card__button').addEventListener('click', openModal);
-            document.getElementById('close').addEventListener('click', closeModal);
+        let params = new URLSearchParams(document.location.search.substring(1));
+        let id = params.get("id");        
+        photographers = data.photographers[i];
 
+        /* Photographer card creation */
+    
+        photographerName.innerText = photographers.name;
+        photographerLocation.innerText = photographers.city + ", " + photographers.country;
+        photographerTagline.innerText = photographers.tagline;
+        photographerTags.innerHTML = "";
+        for(j=0;j<photographers.tags.length;j++){
+            photographerTagLink.innerText = photographers.tags[j];
+            hashtag.innerText = "#";
+            photographerTagLink.prepend(hashtag);
+            photographerTag.appendChild(photographerTagLink);
+            photographerTags.appendChild(photographerTag.cloneNode(true));
+        }
+        photographerButton.innerText = "Contactez-moi";
+        photographerPortrait.src = "../images/pictures/portraits/" + photographers.portrait;
+        document.querySelector('main').appendChild(photographerCard.cloneNode(true));
+
+        /* Form modal creation */
+        const form = document.createElement("form");
+        form.id = "photographer-form";
+        const formHeading = document.createElement("h1");
+        const formFirstLabel = document.createElement("label");
+        formFirstLabel.setAttribute('for', 'first');
+        const formFirstInput = document.createElement("input");
+        formFirstInput.setAttribute('type', 'text');
+        formFirstInput.id = "first";
+        formFirstInput.setAttribute('name', 'first');
+        const formLastLabel = document.createElement("label")
+        formLastLabel.setAttribute('for', 'last');
+        const formLastInput = document.createElement("input");
+        formLastInput.setAttribute('type', 'text');
+        formLastInput.id = "last";
+        formLastInput.setAttribute('name', 'last');
+        const formEmailLabel = document.createElement("label")
+        formEmailLabel.setAttribute('for', 'email');
+        const formEmailInput = document.createElement("input");
+        formEmailInput.setAttribute('type', 'text');
+        formEmailInput.id = "email";
+        formEmailInput.setAttribute('name', 'email');
+        const formMessageLabel = document.createElement("label");
+        formMessageLabel.setAttribute('for', 'message');
+        const formMessageInput = document.createElement("textarea");
+        formMessageInput.setAttribute('type', 'text');
+        formMessageInput.id = "message";
+        formMessageInput.setAttribute('name', 'message');
+        const formSubmit = document.createElement("button");
+        formSubmit.id = "submit"
+        const formClose = document.createElement("button");
+        formClose.id = "close"
+
+        form.appendChild(formHeading);
+        form.appendChild(formFirstLabel);
+        form.appendChild(formFirstInput);    
+        form.appendChild(formLastLabel);    
+        form.appendChild(formLastInput);
+        form.appendChild(formEmailLabel);    
+        form.appendChild(formEmailInput);
+        form.appendChild(formMessageLabel);    
+        form.appendChild(formMessageInput);
+        form.appendChild(formSubmit);
+        form.appendChild(formClose);                 
+        document.querySelector('body').appendChild(form);
+        
+        formHeading.innerHTML = "Contactez-moi<br>\ " + photographers.name;
+        formFirstLabel.innerText = "Prénom";
+        formLastLabel.innerText = "Nom";
+        formEmailLabel.innerText = "Email";
+        formMessageLabel.innerText = "Votre message";
+        formSubmit.innerText = "Envoyer";
+        formClose.innerText = "X";
+
+        function openModal(){
+            form.style.display = "flex";
+            document.querySelector('main').style.display = "none";
+            document.querySelector('header').style.display = "none";
+        }
+
+        function closeModal(){
+            form.style.display = "none";
+            document.querySelector('main').style.display = "flex";
+            document.querySelector('header').style.display = "block";
+        }
+    
+        document.getElementById('photographer-card__button').addEventListener('click', openModal);
+        document.getElementById('close').addEventListener('click', closeModal);
+
+        /* Sort-by creation */
+
+
+        
         /* Pictures section creation */
 
         const media = data.media;
@@ -141,18 +153,44 @@ window.onload = function() {
         const sortOptions = ['Popularité', 'Date', 'Titre'];
         const option = document.createElement("option");
         const pictureCard = document.createElement("article");
+        pictureCard.className = "picture-card";
         const pictureLink = document.createElement("a");
+        pictureLink.className = "picture-card__link";
         const picture = document.createElement("img");
-        const pictureInfo = document.createElement("div");
-        const pictureTitle = document.createElement("p");
-        const pictureLikes = document.createElement("img");
+        picture.className = "picture-card__link-image";
+        const pictureInfo = document.createElement("div");        
+        pictureInfo.className = "picture-card__info";
+        const pictureTitle = document.createElement("p");        
+        pictureTitle.className = "picture-card__info-title";
+        const pictureLikesNumber = document.createElement("p");        
+        pictureLikesNumber.className = "picture-card__info-likes";        
+        const pictureLikeIcon = document.createElement("i");        
+        pictureLikeIcon.className = "fas fa-heart";
+        pictureLikeIcon.src = "../images/heart-solid.svg";
 
+        document.querySelector('main').appendChild(sortBy);
         document.querySelector('main').appendChild(picturesSection);
-        picturesSection.appendChild(sortBy);
         sortBy.appendChild(sortByLabel);
         sortBy.appendChild(sortBySelect);        
         for(i=0;i<sortOptions.length;i++){
+            option.innerText = sortOptions[i];
             sortBySelect.appendChild(option.cloneNode(true));
+        }
+        for(i=0;i<media.length;i++){
+
+            if(media[i].photographerId == id){
+                pictureCard.appendChild(pictureLink);
+                pictureLink.appendChild(picture);               
+                picture.src = "../images/pictures/Mimi/" + media[i].image;
+                pictureCard.appendChild(pictureInfo);
+                pictureInfo.appendChild(pictureTitle);
+                pictureTitle.innerText = media[i].title;
+                pictureInfo.appendChild(pictureLikesNumber);
+                pictureLikesNumber.innerText = media[i].likes;
+                pictureLikesNumber.appendChild(pictureLikeIcon);                
+                picturesSection.appendChild(pictureCard.cloneNode(true)); 
+            }
+
         }
     })
 }
